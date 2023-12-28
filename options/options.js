fc213d0cb2603fdbbbb227ece362dd0eaf559567
Crowdbotics <team@crowdbotics.com>
2023-12-28 04:47:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "stalcraft-45212",
+  url: "https://stalcraft-45212.botics.co",
+  api: "https://stalcraft-45212.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
